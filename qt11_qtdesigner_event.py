bed5d6a07779cabb2c13e6411845ee5422e5d64a
Cyril Vincent <contact@cyrilvincent.com>
2022-11-08 09:55:13 +0100

    Teledyne

--- qt11_qtdesigner_event.py
+++ qt11_qtdesigner_event.py
@@ -1,11 +1,3 @@
-# pip install pyqt5-tools
-# Python 3.10 non supporté
-# C:\Users\conta\AppData\Local\Programs\Python\Python39\Lib\site-packages\qt5_applications\Qt\bin
-# Egalement dans Qt/bin
-# Sinon download : https://www.qt.io/download-qt-installer
-# 2 solutions :
-# Créer le .py
-# Créer le .ui
 from PyQt5 import QtWidgets, uic
 import sys
 
@@ -14,6 +6,13 @@ class Ui(QtWidgets.QDialog): # QDialog vient de widget.class dans .ui
         super(Ui, self).__init__() # Call the inherited classes __init__ method
         uic.loadUi('ui/form.ui', self) # Load the .ui file
         self.show() # Show the GUI
+        self.textEdit.setText("Hello")
+        self.pushButton.clicked.connect(self.button_clicked)
+
+    def button_clicked(self):
+        self.textEdit.setText("Clicked")
+
+
 
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
