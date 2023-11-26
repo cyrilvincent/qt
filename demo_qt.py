4c8814e60730139f178291b4698db0aa5e5ef31b
Cyril Vincent <contact@cyrilvincent.com>
2023-11-26 22:21:30 +0100

    PPTs

---
+++ demo_qt.py
@@ -0,0 +1,19 @@
+from PyQt5 import QtWidgets, uic
+import sys
+
+class Ui(QtWidgets.QDialog): # QDialog vient de widget.class dans .ui
+    def __init__(self):
+        super(Ui, self).__init__() # Call the inherited classes __init__ method
+        uic.loadUi('ui/form.ui', self) # Load the .ui file
+        self.show() # Show the GUI
+        self.textEdit.setText("Hello")
+        self.pushButton.clicked.connect(self.button_clicked)
+
+    def button_clicked(self):
+        self.textEdit.setText("Clicked")
+
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
